2e4abbdbef048ebcdbf9a6e7dab96235d7e7a25a
Tomas Hofman <thofman@redhat.com>
2023-01-27 13:17:26 +0100

    Remove the CLI module
    None of the functionalities are currently used, can be resurrected if
    needed.

--- cli/src/main/java/org/wildfly/channeltools/cli/CliLogger.java
+++
@@ -1,8 +0,0 @@
-package org.wildfly.channeltools.cli;
-
-import org.jboss.logging.Logger;
-
-public final class CliLogger {
-
-    public static final Logger LOGGER = Logger.getLogger(CliLogger.class.getPackageName());
-}
--- cli/src/main/java/org/wildfly/channeltools/cli/MainCommand.java
+++
@@ -1,7 +0,0 @@
-package org.wildfly.channeltools.cli;
-
-import picocli.CommandLine;
-
-@CommandLine.Command(name = "wildfly-channel-cli")
-public class MainCommand {
-}
